6dd4bc386619ae2455652bd480499ce463196210
João Paulo Moura Clevelares <joaopaulomoura5328@gmail.com>
2024-08-24 10:34:49 -0300

    :recycle: Improving inspect box

--- client/src/Inspect.tsx
+++ client/src/Inspect.tsx
@@ -4,12 +4,26 @@ import { ethers } from "ethers";
 import configFile from "./config.json";
 import styles from "./styles/Inspect.module.css";
 
+interface data_model {
+  id: string
+  raw_cert: string,
+  public_key: string,
+  country: string,
+  state: string,
+  locality: string,
+  organization: string,
+  common_name: string,
+  active: string,
+  generation_date: string,
+  expiration_date: string
+}
+
 const config: any = configFile; // Carrega as configuracoes de blockchain a partir do arquivo JSON
 
 export const Inspect: React.FC = () => {
   const [{ connectedChain }] = useSetChain(); // Obtem a cadeia de blockchain conectada
   const [inspectData, setInspectData] = useState<string | null>(null); // Estado para armazenar os dados a serem inspecionados
-  const [reports, setReports] = useState<string[]>([]); // Estado para armazenar os relatorios obtidos
+  const [reports, setReports] = useState<data_model[]>([]); // Estado para armazenar os relatorios obtidos
   const [metadata, setMetadata] = useState<any>({}); // Estado para armazenar metadados relacionados
   const [showReport, setShowReport] = useState<boolean>(false); // Controle de exibicao de relatorios
   const [showError, setShowError] = useState<boolean>(false); //Controle mensagens de erro
@@ -44,20 +58,32 @@ export const Inspect: React.FC = () => {
       const fetchData = await fetch(`${apiURL}/${payload}`);
       const data = await fetchData.json();
 
-      // Atualiza os estados
-      setReports(data.reports);
+      let str_payload = ethers.utils.toUtf8String(data.reports[0].payload)
+      str_payload = str_payload.replace(/'/g, '"')
+
+      if (!str_payload.includes("{")) {
+        alert(str_payload)
+        return
+      }
+
+      let parsed_payload: data_model = JSON.parse(str_payload);
+      console.log(parsed_payload)
+      
+      // Atualiza os estados=====================================
+      setReports([parsed_payload]);
       setMetadata({
         metadata: data.metadata,
         status: data.status,
         exception_payload: data.exception_payload,
       });
-
       setShowReport(true); // Exibe os relatorios
       setShowError(false);
+
+
     } catch (error) {
       console.error("Error fetching reports:", error);
       setShowReport(false);
-      alert("Erro de requisição. Confira o arquivo com a chave pública."); // DEBUG
+      alert(`Erro ao inspecionar certificado ${error}`); // DEBUG
     }
   };
 
@@ -118,18 +144,64 @@ export const Inspect: React.FC = () => {
             <h3>Reports</h3>
             <table className={styles.table}>
               <tbody>
-                {reports?.length === 0 && (
+                {reports?.length === 0 ? (
                   <tr>
                     <td colSpan={4} className={styles.noData}>
                       Nenhum relatório disponível
                     </td>
                   </tr>
-                )}
-                {reports?.map((n: any) => (
-                  <tr key={`${n.payload}`} className={styles.tableRow}>
-                    <td>{ethers.utils.toUtf8String(n.payload)}</td>
-                  </tr>
-                ))}
+                ) : (
+                  reports.map((report, index) => (
+                    <React.Fragment key={index}>
+                    <tr className={styles.tableRow}>
+                      <td><b>ID:</b></td>
+                      <td>{report.id}</td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>Raw Cert:</b></td>
+                      <td><pre>{report.raw_cert}</pre></td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>Public Key:</b></td>
+                      <td><pre>{report.public_key}</pre></td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>Country:</b></td>
+                      <td>{report.country}</td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>State:</b></td>
+                      <td>{report.state}</td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>Locality:</b></td>
+                      <td>{report.locality}</td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>Organization:</b></td>
+                      <td>{report.organization}</td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>Common Name:</b></td>
+                      <td>{report.common_name}</td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>Active:</b></td>
+                      <td>{report.active}</td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>Generation Date:</b></td>
+                      <td>{report.generation_date}</td>
+                    </tr>
+                    <tr className={styles.tableRow}>
+                      <td><b>Expiration Date:</b></td>
+                      <td>{report.expiration_date}</td>
+                    </tr>
+                    <tr><td colSpan={2}><hr /></td></tr>
+                  </React.Fragment>
+                  ))  
+                )
+                }
               </tbody>
             </table>
           </div>
